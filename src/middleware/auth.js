2eb4af4ce4f099a8b88d1a4d1da6b8faa3384ec3
mai van nam <maivannam27699@gmai.com>
2024-04-08 18:11:55 +0700

    namzzz

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,23 +1,27 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
-    const authHeader = req.heaher('Authorization');
-    const token = authHeader && authHeader.split(' ')[1];
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
 
-    if(!token){
-        return res.status(401).json({success: false, message: 'Xác thực không hợp lệ!'});
-    }
+  if (!token) {
+    return res
+      .status(401)
+      .json({ success: false, message: "Xác thực không hợp lệ!" });
+  }
 
-    const ACCESS_TOKEN_SECRET = "akjfksbmdvskmfbkswuigsc"; //nen cho vao env
-    try {
-        const decoded = jwt.verify(token, ACCESS_TOKEN_SECRET);
-        req.staffId = decoded.staffId;
-        req.staffType = decoded.type;
-        next();
-    } catch (error) {
-        console.log(error);
-        return res.status(403).json({success:false, message: "Xác thực không hợp lệ!"});
-    }
+  const ACCESS_TOKEN_SECRET = "akjfksbmdvskmfbkswuigsc"; //nen cho vao env
+  try {
+    const decoded = jwt.verify(token, ACCESS_TOKEN_SECRET);
+    req.staffId = decoded.staffId;
+    req.staffType = decoded.type;
+    next();
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(403)
+      .json({ success: false, message: "Xác thực không hợp lệ!" });
+  }
 };
 
-module.exports = {verifyToken};
+module.exports = { verifyToken };
