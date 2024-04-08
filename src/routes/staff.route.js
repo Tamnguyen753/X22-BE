8163fcad8b22b6e9a34911e415e7e94ef4bce5d0
Tamnguyen753 <nguyendactam0299@gmail.com>
2024-04-08 21:04:32 +0700

    update upload file

--- src/routes/staff.route.js
+++ src/routes/staff.route.js
@@ -5,23 +5,24 @@ const {
   Register,
   Login,
   GetStaff,
-  CreateStaffAccount,
-  GetAllStaff,
+  createStaffAccount,
+  getAllStaff,
 } = require("../controler/staff.controller");
 // const { verifyToken } = require('../middleware/auth');
-const { checkTokenAndType } = require("../middleware/CheckTokenAndType");
+// const { checkTokenAndType } = require("../middleware/CheckTokenAndType");
+const { verifyManager, verifyToken } = require("../middleware/auth.middleware");
 
 // StaffRoute.post("/registerStaff", Register); // quản lý nhà hàng đăng ký tài khoản
 staffRoute.post("/loginStaff", Login); // quản lý nhà hàng + nhân viên đăng nhập
 // staffRoute.get("/", verifyToken, GetStaff); // lấy thông tin của quản lý/nhân viên đang đăng nhập
 
 // quản lý nhà hàng lấy danh sách nhân viên của nhà hàng
-staffRoute.get("/", GetAllStaff);
+staffRoute.get("/", getAllStaff);
 
 // quản lý nhà hàng tạo tài khoản cho nhân viên
-staffRoute.post("/", checkTokenAndType, CreateStaffAccount);
+staffRoute.post("/", verifyToken, verifyManager, createStaffAccount);
 
 // quản lý nhà hàng cập nhật tài khoản nhân viên
-staffRoute.put("/:id", checkTokenAndType, CreateStaffAccount);
+staffRoute.put("/:id", verifyToken, verifyManager, createStaffAccount);
 
 module.exports = { staffRoute };
